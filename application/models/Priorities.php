ef42db487d4640e0b794e5618f0b4c022df90589
Daniel Tian <daniel_tian2002@hotmail.com>
2017-02-16 16:19:20 -0800

    added 6 models and configured the autoload.php to reflect that

---
+++ application/models/Priorities.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * @author Daniel Tian
+ */
+
+class Priorities extends MY_Model {
+
+        public function __construct()
+        {
+                parent::__construct('priorities', 'id');
+        }
+
+}
